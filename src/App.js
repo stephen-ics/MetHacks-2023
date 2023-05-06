8a159ce558ea4d4564633efe741504adf75b04e3
stephen-ics <stephenni1234@gmail.com>
2023-05-06 18:13:11 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { AnimatePresence } from 'framer-motion'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
+import Explore from './pages/Explore'
 
 
 const Blog = require('./models/Model');
@@ -20,13 +21,15 @@ function App() {
        <Suspense fallback={<div>Page Loading...</div>}>
          <AnimatePresence
           mode='wait'>
+            <Navbar />
             <Routes>
+              
               <Route path="/" exact element={<Home />} />
               <Route path="/about" exact element={<About />} />
+              <Route path="/explore" exact element={<Explore />} />
             </Routes>
          </AnimatePresence>
        </Suspense>
-       <Navbar />
      </BrowserRouter>
    </div>
  );
