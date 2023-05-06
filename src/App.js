3618e7f5625f16b8059749856f5e557012eab803
areuie <63759859+areuie@users.noreply.github.com>
2023-05-06 17:57:53 -0400

    edited

--- src/App.js
+++ src/App.js
@@ -5,28 +5,26 @@ import { AnimatePresence } from 'framer-motion'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
+import Explore from './pages/Explore'
 
 
 const Blog = require('./models/Model');
-
-
-
-
 
 function App() {
  return (
    <div className="App">
      <BrowserRouter>
+      
        <Suspense fallback={<div>Page Loading...</div>}>
          <AnimatePresence
           mode='wait'>
+            <Navbar></Navbar>
             <Routes>
               <Route path="/" exact element={<Home />} />
-              <Route path="/about" exact element={<About />} />
+              <Route path="/explore" exact element={<Explore />} />
             </Routes>
          </AnimatePresence>
        </Suspense>
-       <Navbar />
      </BrowserRouter>
    </div>
  );
